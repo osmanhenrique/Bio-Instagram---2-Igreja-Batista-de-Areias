30a5539ee4c3ee6ebbcbb66eacdc2a82661cbe22
Osman Henrique <59256663+osmanhenrique@users.noreply.github.com>
2026-05-16 13:30:43 -0300

    feat: Add "Chá com Feminices 2026" link
    Adds a new link for the "Chá com Feminices 2026" event to the application's data. This makes the event registration form accessible to users.

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,11 @@ const LINKS = [
         label: "BOLETIM DOMINICAL", 
         url: "#",
         status: "construção"
+    },
+    { 
+        id: 18, 
+        label: "CHÁ COM FEMINICES 2026", 
+        url: "https://forms.gle/VvD5ckMKHHJXtCQq9" 
     }
 ];
 
